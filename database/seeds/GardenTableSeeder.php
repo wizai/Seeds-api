50abef8014c127791c407bc4ec33ca7dccf36848
wizai <william.nisole@gmail.com>
2020-06-19 15:03:52 +0200

    Update Seeder

--- database/seeds/GardenTableSeeder.php
+++ database/seeds/GardenTableSeeder.php
@@ -12,6 +12,6 @@ class GardenTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(Garden::class, 10)->create();
+        factory(Garden::class, 1)->create();
     }
 }
